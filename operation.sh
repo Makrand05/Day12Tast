1647458f4a2cdc9f0ebb5cdd8d626f10a5030708
Makrand <makrandshingare@gmail.com>
2021-11-13 19:18:06 +0530

    [Makrand second operation calulated]

--- operation.sh
+++ operation.sh
@@ -2,4 +2,8 @@ read -p "Value of a : " a
 read -p "Value of b : " b
 read -p "Value of c : " c
 val=$(( a + b * c ))
- echo $val
+echo "Value of a + b * c = $val"
+
+val2=$(( a * b + c ))
+
+echo "value of a * b + c = $val2"
